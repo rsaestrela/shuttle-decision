af15ea8fc87d68cab63419c4b2040bf50cc60635
Raul Estrela <raul.estrela@blip.pt>
2017-01-11 16:30:21 +0000

    custom exceptions

---
+++ src/main/java/io/github/rsaestrela/shuttle/decisor/exception/ShuttleDecisorIndeterminateResultException.java
@@ -0,0 +1,9 @@
+package io.github.rsaestrela.shuttle.decisor.exception;
+
+
+public class ShuttleDecisorIndeterminateResultException extends Exception {
+
+    public ShuttleDecisorIndeterminateResultException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/io/github/rsaestrela/shuttle/decisor/exception/ShuttleDecisorInstantiationException.java
@@ -0,0 +1,9 @@
+package io.github.rsaestrela.shuttle.decisor.exception;
+
+
+public class ShuttleDecisorInstantiationException extends RuntimeException /* InstantiationException */{
+
+    public ShuttleDecisorInstantiationException(String message) {
+        super(message);
+    }
+}
